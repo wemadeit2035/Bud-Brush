d7a9dbbe44a2cfdda050ae5ad583c6c5ca59fbf8
wemadeit2035 <amoswetu22@gmail.com>
2026-08-06 11:10:29 +0200

    netlify

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Make sure this is set to '/'
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
+  server: {
+    port: 5173,
+  },
 });
