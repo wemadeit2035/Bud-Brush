851c4e9acca40b34e777729bf97bcc57cdc2e197
wemadeit2035 <amoswetu22@gmail.com>
2026-08-07 18:29:14 +0200

    v.2.3.5

--- src/components/POSView.jsx
+++ src/components/POSView.jsx
@@ -15,16 +15,18 @@ export default function POSView({
     <div className="space-y-6">
       <div className="grid gap-6 lg:grid-cols-[2fr_1fr]">
         <ProductGrid products={products} cart={cart} setCart={setCart} />
-        <CartPanel
-          cart={cart}
-          setCart={setCart}
-          products={products}
-          setProducts={setProducts}
-          transactions={transactions}
-          setTransactions={setTransactions}
-          setSyncStatus={setSyncStatus}
-          showToast={showToast}
-        />
+        <div className="lg:sticky lg:top-4 lg:self-start">
+          <CartPanel
+            cart={cart}
+            setCart={setCart}
+            products={products}
+            setProducts={setProducts}
+            transactions={transactions}
+            setTransactions={setTransactions}
+            setSyncStatus={setSyncStatus}
+            showToast={showToast}
+          />
+        </div>
       </div>
     </div>
   );
